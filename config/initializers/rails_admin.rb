93571ceee3456e87b254d0493fe7be603bd96765
La Van Bac <baclv96@gmail.com>
2018-09-24 16:53:10 +0700

    Add rails_admin

---
+++ config/initializers/rails_admin.rb
@@ -0,0 +1,49 @@
+RailsAdmin.config do |config|
+  config.parent_controller = ApplicationController.name
+
+  config.authorize_with do
+    redirect_to main_app.root_path,
+      flash: {danger: "Access Denied"} unless current_user.admin?
+  end
+
+  config.actions do
+    dashboard
+    index
+    new
+    export
+    bulk_delete
+    show
+    edit
+    delete
+  end
+
+  config.model User.name do
+    list do
+      field :id
+      field :name
+      field :login
+      field :email
+      field :role
+      field :chatwork
+    end
+
+    edit do
+      field :id
+      field :avatar
+      field :name
+      field :login
+      field :email
+      field :role
+      field :chatwork
+      field :room_id
+    end
+  end
+
+  config.model Room.name do
+    edit do
+      field :id
+      field :name
+      field :description
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_02_110748) do
+ActiveRecord::Schema.define(version: 2018_09_24_123310) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2018_08_02_110748) do
     t.jsonb "settings"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
     t.index ["chatwork"], name: "index_users_on_chatwork", unique: true
     t.index ["room_id"], name: "index_users_on_room_id"
   end
