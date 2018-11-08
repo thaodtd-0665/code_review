445dee08ae5a424bd0571fcf7d2c99c47cff51c8
La Van Bac <baclv96@gmail.com>
2018-11-08 16:44:29 +0700

    fix connection

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -10,7 +10,10 @@ module ApplicationCable
 
     def find_user
       # TODO: check reviewer
-      user = User.find_by id: cookies.encrypted[:user_id]
+      user_id = cookies.encrypted[:user_id]
+      user_id || reject_unauthorized_connection
+
+      user = User.find_by id: user_id
       user || reject_unauthorized_connection
     end
   end
