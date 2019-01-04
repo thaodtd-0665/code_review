9b71c78a95d730f15ab47577e8b038804a1424af
La Van Bac <la.van.bac@framgia.com>
2019-01-04 11:35:17 +0700

    check all pr

--- app/workers/conflict_worker.rb
+++ app/workers/conflict_worker.rb
@@ -3,10 +3,7 @@ class ConflictWorker
   sidekiq_options retry: 1
 
   def perform repo_id
-    pull_request = PullRequest.where(repository_id: repo_id, state: :ready)
-                              .order(number: :asc)
-                              .first
-    return unless pull_request
-    ConflictService.call pull_request
+    PullRequest.where(repository_id: repo_id, state: :ready)
+               .each{|request| ConflictService.call request}
   end
 end
